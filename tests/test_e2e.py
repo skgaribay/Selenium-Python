2251d1ae93124c30380f20b07360ae16f752b87a
Sean Kristian Garibay <krisg177@gmail.com>
2024-06-26 23:15:54 +0800

    Driver manager

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -3,11 +3,16 @@ import pytest
 # import os
 from selenium import webdriver
 from pages.page_object_manager import PageObjectManager
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture(scope="function")
 def setup(request):
-    driver = webdriver.Chrome()
+    options = Options()
+    options.add_argument('--headless')
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     driver.get("https://www.saucedemo.com/")
     driver.maximize_window()
     request.cls.page_manager = PageObjectManager(driver)
